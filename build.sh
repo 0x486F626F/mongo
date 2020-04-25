bd038c9e5a8a3d77f90ac3e7642e71f19cec9c9d
Determinant <tederminant@gmail.com>
2020-04-25 18:11:20 +0000

    improve the script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
+PREFIX="${PREFIX:-./mongo_build}"
 cd rocksdb
-USE_RTTI=1 CFLAGS=-fPIC make shared_lib -j8; sudo INSTALL_PATH=/usr make install;
+USE_RTTI=1 CFLAGS=-fPIC make shared_lib -j8; INSTALL_PATH="$PREFIX" make install;
 cd ..
 scons mongod -j8 --disable-warnings-as-errors
 
